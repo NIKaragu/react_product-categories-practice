7e5dd14b164adc1a307f17d88fa26f14e706c073
Nikita <perfect1201@ukr.net>
2024-08-01 15:10:19 +0300

    fixed ClearButton in search field

--- src/App.jsx
+++ src/App.jsx
@@ -152,14 +152,19 @@ export const App = () => {
                   <i className="fas fa-search" aria-hidden="true" />
                 </span>
 
-                <span className="icon is-right">
-                  {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-                  <button
-                    data-cy="ClearButton"
-                    type="button"
-                    className="delete"
-                  />
-                </span>
+                {searchQuery.length > 0 && (
+                  <span className="icon is-right">
+                    {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                    <button
+                      data-cy="ClearButton"
+                      type="button"
+                      className="delete"
+                      onClick={() => {
+                        setSearchQuery('');
+                      }}
+                    />
+                  </span>
+                )}
               </p>
             </div>
 
